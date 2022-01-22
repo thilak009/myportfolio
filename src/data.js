b8e7d26e5519a977bfd3b925d6b1c252d86f336b
thilak reddy <thilak009reddy@gmail.com>
2022-01-22 10:16:43 +0530

    removed a project from list

--- src/data.js
+++ src/data.js
@@ -1,62 +1,58 @@
-import cocktail from './images/cocktails-project.jpg';
-import stripeMenu from './images/stripe-menu.png';
-import githubUsers from './images/github-users.png';
-import crypto from './images/crypto.png';
-import discord from './images/discord-clone.png';
+import cocktail from "./images/cocktails-project.jpg";
+import stripeMenu from "./images/stripe-menu.png";
+import githubUsers from "./images/github-users.png";
+import crypto from "./images/crypto.png";
+import discord from "./images/discord-clone.png";
 // import earth from './images/earth.png'
-import resort from './images/beach-resort.png'
-import tomodachi from './images/tomodachides.png'
+import resort from "./images/beach-resort.png";
+import tomodachi from "./images/tomodachides.png";
 
-const data=[
-    {
-        image: tomodachi,
-        title: "Social Media App",
-        description: "A social networking app where one can post,comment and interact with people by following and chatting",
-        liveLink: "https://tomodachides.netlify.app/",
-        githubLink: "https://github.com/thilak009/social-media-app-frontend"
-    },
-    {
-        image: githubUsers,
-        title: "Github Users Search",
-        description: "Search github users and their data is represenred in charts",
-        liveLink: "https://github-userinfo.netlify.app/",
-        githubLink: "https://github.com/thilak009/github-users-search"
-    },
-    {
-        image: resort,
-        title: "Resort Website",
-        description: "A resort website with contentful CMS to update data easily without knowledge of code", 
-        liveLink: "https://beach-resort-cms.netlify.app/",
-        githubLink: "https://github.com/thilak009/react-beach-resort"
-    },
-    {
-        image: discord,
-        title: "Discord Clone",
-        description: "Simple chat application like discord where you can create channels and chat",
-        liveLink: "https://discord-clone-7449c.web.app/",
-        githubLink: ""
-    },
-    {
-        image: cocktail,
-        title: "Cocktail Search",
-        description: "Search for different cocktails made with CocktailDB API",
-        liveLink: "https://cocktail-react-project.netlify.app/",
-        githubLink: "https://github.com/thilak009/cocktail-search"
-    },
-    {
-        image: crypto,
-        title: "Crypto Tracker",
-        description: "Get Cryptocurrencies details like price,market cap etc",
-        liveLink: "https://gocrypto.netlify.app/",
-        githubLink: "https://github.com/thilak009/go_crypto"
-    },
-    {
-        image: stripeMenu,
-        title: "Stripe Menu Bar",
-        description: "Navbar design inspired from the stripe website,made with react",
-        liveLink: "https://stripe-menu-bar.netlify.app/",
-        githubLink: "https://github.com/thilak009/stripe-menu"
-    }
-]
+const data = [
+  {
+    image: tomodachi,
+    title: "Social Media App",
+    description:
+      "A social networking app where one can post,comment and interact with people by following and chatting",
+    liveLink: "https://tomodachides.netlify.app/",
+    githubLink: "https://github.com/thilak009/social-media-app-frontend",
+  },
+  {
+    image: githubUsers,
+    title: "Github Users Search",
+    description: "Search github users and their data is represenred in charts",
+    liveLink: "https://github-userinfo.netlify.app/",
+    githubLink: "https://github.com/thilak009/github-users-search",
+  },
+  {
+    image: resort,
+    title: "Resort Website",
+    description:
+      "A resort website with contentful CMS to update data easily without knowledge of code",
+    liveLink: "https://beach-resort-cms.netlify.app/",
+    githubLink: "https://github.com/thilak009/react-beach-resort",
+  },
+  {
+    image: cocktail,
+    title: "Cocktail Search",
+    description: "Search for different cocktails made with CocktailDB API",
+    liveLink: "https://cocktail-react-project.netlify.app/",
+    githubLink: "https://github.com/thilak009/cocktail-search",
+  },
+  {
+    image: crypto,
+    title: "Crypto Tracker",
+    description: "Get Cryptocurrencies details like price,market cap etc",
+    liveLink: "https://gocrypto.netlify.app/",
+    githubLink: "https://github.com/thilak009/go_crypto",
+  },
+  {
+    image: stripeMenu,
+    title: "Stripe Menu Bar",
+    description:
+      "Navbar design inspired from the stripe website,made with react",
+    liveLink: "https://stripe-menu-bar.netlify.app/",
+    githubLink: "https://github.com/thilak009/stripe-menu",
+  },
+];
 
-export default data
+export default data;
